0db13953fefcc1b77b6f45f4934246d834bfc7f5
thoai112 <40832268+thoai112@users.noreply.github.com>
2022-05-31 17:19:29 +0700

    Update index.jsx

--- src/Pages/Login/index.jsx
+++ src/Pages/Login/index.jsx
@@ -70,7 +70,7 @@ const Login = () => {
 
 						{error && <div className={styles.error_msg}>{error}</div>}
 						<button type="submit" className={styles.green_btn}>
-							Sing In
+							Sign In
 						</button>
 					</form>
 				</div>
@@ -78,7 +78,7 @@ const Login = () => {
 					<h1>New Here ?</h1>
 					<Link to="/register">
 						<button type="button" className={styles.white_btn}>
-							Sing Up
+							Sign Up
 						</button>
 					</Link>
 				</div>
